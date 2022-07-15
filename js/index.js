010f2d95c2f0096bf0bc4d2fcc0334da732b97b1
Failproo <aryanrajapandey@gmail.com>
2022-07-15 19:56:39 +0530

    added php

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
-let menu = document.querySelector('#menu-icon');
+//init
 
+let menu = document.querySelector('#menu-icon');
+let form = document.querySelector('form');
+let btn = document.querySelector('#submit')
 
 let navbar = document.querySelector('.navbar');
 
+//listener
 menu.onclick = () => {
   
   menu.classList.toggle('bx-x');
@@ -26,6 +30,26 @@ const sr = ScrollReveal( {
 sr.reveal('.home-text', {delay: 200, origin: 'top'});
 sr.reveal('.home-img', { delay: 400, origin: 'top'});
 sr.reveal('.about, .services, .cta, .resume , .contact, .copyright', { delay: 200, origin: "bottom"});
+
+
+//form
+form.onsubmit = (e) => {
+  e.preventDefault();
+  
+  let chr = new XMLHttpRequest();
+  chr.open("POST", "../assets/data/g.php", true);
+  chr.onload = () => {
+    if (chr.readyState === 4 && chr.status === 200) {
+      console.log(`response Status: ${chr.status} && ${chr.readyState}`);
+      let response = chr.response;
+     console.log(response);
+    }
+  };
+  
+  let formdata = new FormData(form);
+  chr.send(formdata);
+};
+
 
 
 
